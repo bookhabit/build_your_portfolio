0245bd51317ab925655b9a6529f18a94fff808ec
bookhabit <qnrgoqlt@naver.com>
2023-06-17 17:42:40 +0900

    헤더 컴포넌트 퍼블리싱 변경

--- front/src/Header.tsx
+++ front/src/Header.tsx
@@ -18,13 +18,13 @@ export default function Header() {
         <input type="text" className="border bg-gray-50 border-header_bg rounded-xl h-8 w-full px-5 text-header_element"/>
       </div>
       {user ? (
-        <div className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 cursor-pointer " onClick={()=>router('/account')}>
+        <div className="flex items-center gap-2 bg-header_element rounded-full py-2 px-4 cursor-pointer " onClick={()=>router('/account')}>
           <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
               <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
             </svg>
           </div>
-          <div>
+          <div className="text-header_bg">
             {user.name}
           </div>
         </div>
