ae1f0f88b26d41492f18513da90dcae46b47fb51
bookhabit <qnrgoqlt@naver.com>
2023-06-24 22:06:48 +0900

    포트폴리오UI 이미지  미리보기 기능

--- front/src/pages/PortfolioFormPage.tsx
+++ front/src/pages/PortfolioFormPage.tsx
@@ -73,7 +73,10 @@ export default function PortfolioFormPage() {
       name:"C",
       src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvb65u-m8PLHvYEJKyQnUKO846ERarRhJzaMfl3d1Rktsf0TMlCQ4xj48uVbKROQpR11U&usqp=CAU"
     }]
-    
+
+    // image-view-state
+    const [showPreview,setShowPreview] = useState<boolean>(false)
+    const [showPreviewSrc,setShowPreviewSrc] = useState<string>("")
 
     // error handling
     const [errorMessage,setErrorMessage] = useState<ValidatePortfolio>({
@@ -202,6 +205,28 @@ export default function PortfolioFormPage() {
         }))
       }
       setValidateMode(false)
+    }
+
+    // image-view
+    if (showPreview&&showPreviewSrc) {
+      return (
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
+          <div>
+            <button onClick={() => setShowPreview(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+              </svg>
+              Close Preview
+            </button>
+          </div>
+          <img 
+            src={showPreviewSrc} 
+            alt="이미지 미리보기"  
+            />
+        </div>
+      </div>
+      );
     }
 
     // validation
@@ -599,7 +624,10 @@ export default function PortfolioFormPage() {
                       src={PreviewIcon} 
                       alt="이미지 확대"  
                       className="absolute right-0 bottom-0"
-                      onClick={()=>console.log('클릭')}
+                      onClick={()=>{
+                        setShowPreview(true)
+                        setShowPreviewSrc(kind.src)
+                      }}
                       />
                     </label>
                 ))}
