fb652cf44728638eaefc264cb337b85adfdf5f7a
bookhabit <qnrgoqlt@naver.com>
2023-07-07 00:00:15 +0900

    포트폴리오 슬라이드 UI 완성

--- front/src/components/PortfolioUI/SlideUI.tsx
+++ front/src/components/PortfolioUI/SlideUI.tsx
@@ -50,23 +50,6 @@ const SlideUI = ({portfolio,userPage}:IProps) => {
             </div>
         </div>
     )
-    const slide4 = (
-            <div className="w-full h-full flex flex-col gap-5">
-                {portfolio.PortfolioDoc.important_functions && portfolio.PortfolioDoc.important_functions.length > 0 &&portfolio.PortfolioDoc.important_functions.map((importantData,index)=>(
-                <div className="w-full h-full flex flex-col gap-5" key={index}>
-                    <div className="w-full flex items-center justify-center lg:justify-start px-20 py-10 h-20 mt-10">
-                        <h2 className="text-3xl font-bold">프로젝트 핵심기능 {index+1}</h2>
-                    </div>
-                    <div className="w-full flex flex-col lg:flex-row gap-5 justify-evenly items-center mb-10 px-20 h-full">
-                        <div className="w-full sm:w-1/3">
-                            {portfolio.PortfolioDoc.photos[2] && <ImageUI className="border shadow-2xl h-full w-full object-fill aspect-square" src={importantData.important_function_photo[0]} />}
-                        </div>
-                        <p className="leading-10 pr-10 w-full lg:w-1/2">{importantData.important_function_desc}</p>    
-                    </div>
-                </div>
-                ))}
-        </div>
-    )
     const slide5 = (
         <div className="w-full h-full bg-codingMan text-white ">
                 <div className="h-full flex flex-col gap-10 items-center justify-center px-5 sm:px-40 bg-black/70">
@@ -139,9 +122,36 @@ const SlideUI = ({portfolio,userPage}:IProps) => {
             </div>
         </div>
     )
+    const slides = [
+        slide1,
+        slide2,
+        slide3,
+        ...portfolio.PortfolioDoc.important_functions.map((importantData, index) => (
+          <div className="w-full h-full flex flex-col gap-5" key={index}>
+            <div className="w-full flex items-center justify-center lg:justify-start px-20 py-10 h-20 mt-10">
+              <h2 className="text-3xl font-bold">프로젝트 핵심기능 {index + 1}</h2>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row gap-5 justify-evenly items-center mb-10 px-20 h-full">
+              <div className="w-full sm:w-1/3">
+                {portfolio.PortfolioDoc.photos[2] && (
+                  <ImageUI
+                    className="border shadow-2xl h-full w-full object-fill aspect-square"
+                    src={importantData.important_function_photo[0]}
+                  />
+                )}
+              </div>
+              <p className="leading-10 pr-10 w-full lg:w-1/2">{importantData.important_function_desc}</p>
+            </div>
+          </div>
+        )),
+        slide5,
+        slide6,
+        slide7,
+      ];
+      
     return (
         <div className="px-0 xl:px-80 h-screen">
-            <Slideshow slides={[slide1,slide2,slide3,slide4,slide5,slide6,slide7]}/>
+            <Slideshow slides={slides}/>
             {userPage&&
             <div className="pb-20">
                 <button className="bg-black text-white p-3 rounded-lg" onClick={()=>router(`/portfolio/update/${portfolio.PortfolioDoc._id}`)}>포트폴리오 수정하기</button>
