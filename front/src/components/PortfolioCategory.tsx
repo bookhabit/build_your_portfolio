d8bcd7942f534da9123e9f0762902ab2aa0c6ddd
bookhabit <qnrgoqlt@naver.com>
2023-07-21 16:41:25 +0900

    가로정렬

--- front/src/components/PortfolioCategory.tsx
+++ front/src/components/PortfolioCategory.tsx
@@ -13,7 +13,7 @@ const PortfolioCategory = ({categoryName,portfolio}:IProps) => {
     return (
         <div className={portfolioCategoryCSS()}>
             <p className="text-neutral-500 text-2xl font-bold">{categoryName}</p>
-            <div className="flex gap-8 flex-wrap">
+            <div className="flex justify-center gap-8 flex-wrap">
                 {
                 portfolio?.map((portfolio)=>(
                     <PortfolioImage key={portfolio._id} portfolio={portfolio}/>
