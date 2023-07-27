73d443f9dedb77cbd2898bc59125cb40ac909f51
bookhabit <qnrgoqlt@naver.com>
2023-07-28 01:35:09 +0900

    백엔드url변경

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -13,7 +13,7 @@ import PortfolioPage from './pages/PortfolioPage';
 import {RecoilRoot} from "recoil"
 
 const LOCAL_BACKEND="http://localhost:4000"
-const DEPLOY_BACKEND="http://build-your-portfolio.de:4000"
+const DEPLOY_BACKEND="https://build-your-portfolio.de"
  
 axios.defaults.baseURL = DEPLOY_BACKEND
 axios.defaults.withCredentials = true;
--- front/src/components/common/ImageUI.tsx
+++ front/src/components/common/ImageUI.tsx
@@ -1,6 +1,6 @@
 export default function ImageUI({src,alt,...rest}:{src:string,alt:string,className?:string}) {
   const LOCAL_BACKEND="http://localhost:4000/uploads/"
-  const DEPLOY_BACKEND="http://54.180.155.198:4000//uploads/"
+  const DEPLOY_BACKEND="https://build-your-portfolio.de/uploads/"
     src = DEPLOY_BACKEND+src;
     return (
       <img {...rest} src={src} alt={alt} />
